782cb584f6c9aa21c819ea6512c75ea0d116ae8c
chivier <chivier.humber@gmail.com>
2020-04-21 17:34:35 +0800

    Update a new test on judge module.

--- backend/judge/test_judge.py
+++ backend/judge/test_judge.py
@@ -18,7 +18,8 @@ class QuestionModelTests(TestCase):
     def test_case_0(self):
         print("\nTest 0:")
         print("Hello world")
-        subprocess.run("pwd")
+        str = subprocess.check_call("pwd")
+        print(str)
 
     def test_case_1_generate(self):
         print("\nTest 1:")
@@ -29,6 +30,6 @@ class QuestionModelTests(TestCase):
         subprocess.run(
             "rm" + " -rf {foldername}".format(foldername=name), shell=True)
 
-    def test_case_2_iverilog_test(self):
-        print("\nTest 2:")
-        perform_judge_iverilog_version(None)
+    # def test_case_2_iverilog_test(self):
+    #     print("\nTest 2:")
+    #     perform_judge_iverilog_version(None)
