7af086aa1944b1be4b1949d14967d658669bae78
sakura <1212195866@qq.com>
2020-06-13 19:20:05 +0800

    frontend/wiki

--- fontend/YAVG_FE/src/components/mainpage/wiki.vue
+++ fontend/YAVG_FE/src/components/mainpage/wiki.vue
@@ -1,48 +1,42 @@
 <template>
   <el-card shadow="always" id="card">
     <center>
-      <h1>欢迎来到某大型在线游戏攻略网站LPOJ Wiki</h1>
-      <h2>在这里你可以找到算法竞赛的学习教程，代码模板，和算法试炼</h2>
+      <h1>欢迎来到USTC Verilog-OJ Wiki</h1>
+      <h2>在这里你可以学习Vrilog的基础知识，vivado的实用教程</h2>
       <h2>现在让我们开始吧！</h2>
     </center>
     <el-row :gutter="10">
       <el-col :span="8">
         <el-card>
           <div slot="header">
-            <b>算法教程</b>
+            <b>verilog基础语法</b>
           </div>
-          <div style=" margin-bottom: 18px;">在这里你可以找到最全的算法攻略</div>
-          <div style=" margin-bottom: 18px;">在这里你可以编写自己的攻略</div>
-          <div style=" margin-bottom: 18px;">在这里你可以查看别人编写的攻略</div>
+          <div style=" margin-bottom: 18px;">这里有基础的Verilog语法，可以帮助你完成实验</div>
           <div style=" margin-bottom: 18px;">
-            <el-button @click="glClick" type="primary">查看攻略</el-button>
-            <el-button @click="jiubanClick" type="text">进入旧版</el-button>
+            <el-button @click="glClick" type="primary">点击学习</el-button>
+          <!--  <el-button @click="jiubanClick" type="text">进入旧版</el-button> -->
           </div>
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card>
           <div slot="header">
-            <b>模板大全</b>
+            <b>vivado基本操作</b>
           </div>
-          <div style=" margin-bottom: 18px;">在这里你可以找到最全的算法模板</div>
-          <div style=" margin-bottom: 18px;">在这里你可以保存自己的模板</div>
-          <div style=" margin-bottom: 18px;">在这里你可以查看别人编写的模板</div>
+          <div style=" margin-bottom: 18px;">这里有一些vivado的基本操作，可以帮助你入门</div>
           <div style=" margin-bottom: 18px;">
-            <el-button @click="mbClick" type="primary">查看模板</el-button>
+            <el-button @click="mbClick" type="primary">点击学习</el-button>
           </div>
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card>
           <div slot="header">
-            <b>LPOJ试炼谷</b>
+            <b>优秀作品</b>
           </div>
-          <div style=" margin-bottom: 18px;">在这里你可以找到ACM的入门方法</div>
-          <div style=" margin-bottom: 18px;">在这里你可以得到完整的历练</div>
-          <div style=" margin-bottom: 18px;">在这里你可以看到自己变成大神的轨迹</div>
+          <div style=" margin-bottom: 18px;">这里有优秀的大作业模板，仅供展示</div>
           <div style=" margin-bottom: 18px;">
-            <el-button @click="trainningClick" type="primary">开始试炼</el-button>
+            <el-button @click="trainningClick" type="primary">点击查看</el-button>
           </div>
         </el-card>
       </el-col>
