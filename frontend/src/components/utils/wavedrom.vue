793ca58e75b99cbeba2a0c0ccf44effd3cdcf7f3
Libre Liu <libreliu@foxmail.com>
2020-09-04 21:27:13 +0000

    Fix bug: Delays one tick to ensure DOM created
    When uses v-for, the DOM was not exist when text changed, and here comes the
    bug.

--- frontend/src/components/utils/wavedrom.vue
+++ frontend/src/components/utils/wavedrom.vue
@@ -22,23 +22,33 @@ export default {
     }
   },
   watch: {
-    text: function() {
-      try {
-        if (this.text == "") {
-            console.log("asdfasdfasdfasdfasdfasdfasdf")
-            throw 'Waveform data empty';
-        } 
-        WaveDrom.renderWaveForm(this.waveId, JSON.parse(this.text), 'waveform');
-        this.isError = false;
-      } catch (e) {
-        console.warn(e);
-        this.isError = true;
-      }
+    text: {
+      handler: function (newVal, oldVal) {
+        // Delay rendering until real DOM created, 
+        // or WaveDrom will fail to get the desired element
+        this.$nextTick(function () {
+          // Code that will run only after the
+          // entire view has been re-rendered
+          try {
+            if (this.text == "") {
+                throw 'Waveform data empty';
+            }
+            // console.log(this.text)
+            // console.log(this.waveId)
+            WaveDrom.renderWaveForm(this.waveId, JSON.parse(this.text), 'waveform');
+            this.isError = false;
+          } catch (e) {
+            console.warn(e);
+            this.isError = true;
+          }
+        })
+      },
+      immediate: true
     },
     parentText: function() {
       this.text = this.parentText;
     }
-  }
+  },
 }
 </script>
 
