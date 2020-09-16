1ae5ef045b345fec74d8b5196751854fd89410c0
Libre Liu <libreliu@foxmail.com>
2020-09-16 03:54:10 +0000

    Problem list speed optimization

--- backend/problem/views.py
+++ backend/problem/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Problem, TestCase
-from .serializers import ProblemSerializer, TestCaseSerializer
+from .serializers import ProblemSerializer, TestCaseSerializer, ProblemAdvancedListSerializer
 from user import permissions
 
 class ProblemViewSet(ModelViewSet):
@@ -9,7 +9,22 @@ class ProblemViewSet(ModelViewSet):
     获取和修改题目信息
     """
     queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
+    #serializer_class = ProblemSerializer
+
+    def get_serializer_class(self):
+        """
+        Override parent code from rest_framework.generics.GenericAPIView
+        to dynamically adjust the item to be used
+        """
+        if not hasattr(self.request, 'user'): # 生成文档用
+            return ProblemSerializer
+        elif self.request.method == 'GET' and (not 'pk' in self.kwargs):
+            # Check if we're querying a specific one
+            # In list mode, the log and app_data is always hidden
+            return ProblemAdvancedListSerializer
+        else:
+            return ProblemSerializer
+
     permission_classes = (permissions.GetOnlyPermission,)
 
 class TestCaseViewSet(ModelViewSet):
