dfc98cd43c06cda61f3b14b7324306f2c59d055f
luckyydog <zhanghaowen@mail.ustc.edu.cn>
2021-01-24 20:54:55 +0800

    add edit

--- frontend/src/components/problem/addproblem.vue
+++ frontend/src/components/problem/addproblem.vue
@@ -4,16 +4,38 @@
       <el-row>
         <el-card shadow="always">
           <!--标题、DDL-->
-          <el-row :gutter="18" id="ddl" >Title</el-row>
-          <el-row :gutter="18" id="title">
-            <el-col :span="20">
-              <el-input v-model="title"></el-input>
-              
-            </el-col>
+          <el-row gutter="18">
             <el-col :span="2">
               <el-button type="success"
                          @click="problemedit"
                          style="font-weight:bold;margin-left:10px;">Submit</el-button>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="danger"
+                         @click="is_change=false"
+                         style="font-weight:bold;margin-left:30px;" v-if="is_change">Cancel</el-button>
+              <el-button v-else type="danger"
+                         @click="is_change=true"
+                         style="font-weight:bold;margin-left:30px;">Edit</el-button>
+            </el-col>
+          </el-row>
+          <el-row :gutter="18" 
+            class="problem-description-title" v-if="is_change">select problem</el-row>
+          <el-row :gutter="18" 
+            class="problem-description-title" v-if="is_change">
+              <el-select v-model="problem_id" placeholder="题目：">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-row>
+          <el-row :gutter="18" id="ddl" >Title</el-row>
+          <el-row :gutter="18" id="title">
+            <el-col :span="20">
+              <el-input v-model="title"></el-input>
             </el-col>
           </el-row>
           <el-row :gutter="18" id="ddl" >Deadline</el-row>
@@ -180,7 +202,10 @@ export default {
       addtime: "",
       ddl: "",
       have_ddl: false,
-    
+      is_change: false,
+      options:[],
+      problem_id:0,
+
       tagnames: [],
       activeNames: ["1", "2", "3", "4", "5", "6"],
       level: 1,
@@ -233,113 +258,17 @@ export default {
   created () {
       this.content =  this.code_items[0].code;
 
-  //   this.$axios
-  //     .get("/problems/" + this.id + "/")
-  //     .then(response => {
-  //       this.des = response.data.description;
-  //       this.input = response.data.description_input;
-  //       this.output = response.data.description_output;
-  //       this.waveform = response.data.app_data;
-  //       // this.author = response.data.author;
-  //       this.addtime = response.data.create_time =
-  //         moment(response.data.create_time).format("YYYY-MM-DD HH:mm:ss");
+      this.$axios.get("/problems/?owner=" + sessionStorage.userid)
+      .then(response => {
+          var problems = response.data;
+          for (var i=0;i<problems.length;i=i+1) {
+            this.options.push({
+              "value": problems[i]["id"],
+              "label": problems[i]["id"]+" "+problems[i]["name"],
+            })
+          }
+        });
 
-  //       this.code = "";
-  //       if (response.data.template_code_file != null) {
-  //         this.$axios.get("/files/" + response.data.template_code_file + "/")
-  //         .then(response => {
-  //           this.code = response.data;
-  //         })
-  //       }
-
-  //       // var li = response.data.template.split("*****")
-  //       // for (var i = 1; i < li.length; i += 2) {
-  //       //   this.codetemplate[li[i]] = li[i + 1]
-  //       // }
-  //       // this.code = this.codetemplate[this.language]
-
-  //       // this.$axios
-  //       //   .get("/problemdata/" + this.ID + "/")
-  //       //   .then(response => {
-  //       if (response.data["level"] == "1") response.data["level"] = "Easy";
-  //       if (response.data["level"] == "2")
-  //         response.data["level"] = "Medium";
-  //       if (response.data["level"] == "3") response.data["level"] = "Hard";
-  //       if (response.data["level"] == "4")
-  //         response.data["level"] = "VeryHard";
-  //       if (response.data["level"] == "5")
-  //         response.data["level"] = "ExtremelyHard";
-  //       // response.data.level = "Easy";
-
-  //       if (response.data["tags"] == null) response.data["tags"] = ["无"];
-  //       else response.data["tags"] = response.data["tags"].split("|");
-  //       // response.data.tag = ["无"];
-
-  //       // if (response.data.submission == 0) {
-  //       //   this.ac = 0;
-  //       //   this.mle = 0;
-  //       //   this.tle = 0;
-  //       //   this.rte = 0;
-  //       //   this.pe = 0;
-  //       //   this.ce = 0;
-  //       //   this.wa = 0;
-  //       //   this.se = 0;
-  //       // } else {
-  //       //   this.ac = parseFloat(
-  //       //     ((response.data.ac * 100) / response.data.submission).toFixed(2)
-  //       //   );
-  //       //   this.mle = parseFloat(
-  //       //     ((response.data.mle * 100) / response.data.submission).toFixed(
-  //       //       2
-  //       //     )
-  //       //   );
-  //       //   this.tle = parseFloat(
-  //       //     ((response.data.tle * 100) / response.data.submission).toFixed(
-  //       //       2
-  //       //     )
-  //       //   );
-  //       //   this.rte = parseFloat(
-  //       //     ((response.data.rte * 100) / response.data.submission).toFixed(
-  //       //       2
-  //       //     )
-  //       //   );
-  //       //   this.pe = parseFloat(
-  //       //     ((response.data.pe * 100) / response.data.submission).toFixed(2)
-  //       //   );
-  //       //   this.ce = parseFloat(
-  //       //     ((response.data.ce * 100) / response.data.submission).toFixed(2)
-  //       //   );
-  //       //   this.wa = parseFloat(
-  //       //     ((response.data.wa * 100) / response.data.submission).toFixed(2)
-  //       //   );
-  //       //   this.se = parseFloat(
-  //       //     ((response.data.se * 100) / response.data.submission).toFixed(2)
-  //       //   );
-  //       // }
-
-  //       if (response.data.deadline_time == null) {
-  //         this.ddl = "";
-  //         this.have_ddl = false;
-  //       } else {
-  //         this.ddl_time = response.data.deadline_time;
-  //         let ddlTime = moment(response.data.deadline_time);
-  //         this.ddl = ddlTime.format("YYYY-MM-DD hh:mm:ss") + " (" + ddlTime.endOf('day').fromNow() + ")";
-  //         //this.ddl = response.data.deadline_time;
-  //         this.have_ddl = true;
-  //       }
-
-  //       this.title = response.data.name;
-  //       this.level = response.data.level;
-  //       this.tagnames = response.data.tags;
-  //       this.submissions = response.data.submissions;
-  //       this.submissions_refresh();
-
-  //       this.$refs.prosta.setdata(this.$data);
-
-  //     })
-  //     .catch(error => {
-  //       this.$message.error("服务器错误！" + "(" + JSON.stringify(error.response.data) + ")");
-  //     });
   },
   methods: {
     /*
@@ -538,6 +467,10 @@ export default {
       }).then(response => {
         alert(response.data);
       })
+    },
+    problemchange() {
+      this.is_change = true;
+      this.$axios
     },
   },
   destroyed () {
