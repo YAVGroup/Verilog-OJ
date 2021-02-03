06544656349de508aa34987c0c47f177f895c4b7
Zhao Zuohong <1040110848@qq.com>
2021-02-03 09:09:10 +0800

    路由调整

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+
 // Regarding "() => import(...)" stuff, check out this:
 // https://router.vuejs.org/guide/advanced/lazy-loading.html#grouping-components-in-the-same-chunk
 
