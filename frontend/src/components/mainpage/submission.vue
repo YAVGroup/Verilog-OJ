d1f6a36c033017472513b17077b4eb60977893d1
Zhao Zuohong <1040110848@qq.com>
2021-08-03 22:20:40 +0800

    修复提交时间相差8小时前的错误

--- frontend/src/components/mainpage/submission.vue
+++ frontend/src/components/mainpage/submission.vue
@@ -179,29 +179,6 @@ export default {
       document.body.removeChild(downloadElement); //下载完成移除元素
       window.URL.revokeObjectURL(href); //释放掉blob对象
     },
-    prettyDate(time) {
-      let date = new Date(
-          (time || "").replace(/-/g, "/").replace(/[TZ]/g, " ")
-        ),
-        diff = (new Date().getTime() - date.getTime()) / 1000,
-        day_diff = Math.floor(diff / 86400);
-
-      // return date for anything greater than a day
-      if (isNaN(day_diff) || day_diff < 0 || day_diff > 0)
-        return date.getDate() + " " + date.toDateString().split(" ")[1];
-
-      return (
-        (day_diff == 0 &&
-          ((diff < 60 && "刚刚") ||
-            (diff < 120 && "1 分钟前") ||
-            (diff < 3600 && Math.floor(diff / 60) + " 分钟前") ||
-            (diff < 7200 && "1 小时前") ||
-            (diff < 86400 && Math.floor(diff / 3600) + " 小时前"))) ||
-        (day_diff == 1 && "昨天") ||
-        (day_diff < 7 && day_diff + " 天前") ||
-        (day_diff < 31 && Math.ceil(day_diff / 7) + " 周前")
-      );
-    },
 
     updateStatus() {
       this.$axios
@@ -215,6 +192,7 @@ export default {
           this.num_testcase = this.results.length;
           this.related_testcases = response.data.problem_belong.testcases;
           this.submit_time = new Date(response.data.submit_time);
+          console.log(this.submit_time.toISOString());
           this.subm_userid = response.data.user;
 
           let passed = 0;
@@ -289,7 +267,8 @@ export default {
   computed: {
     submitTimePretty: function () {
       let tm = this.submit_time;
-      return this.prettyDate(tm.toISOString());
+      let duration = moment(tm).locale("zh-CN").fromNow();
+      return duration;
     },
     ...mapState(["loggedIn", "userID", "username", "isSuperUser"]),
     needRefresh: function () {
