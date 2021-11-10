bea8def92c6cf6f3ecdfbc8e7c96cb07a876b11c
Libre Liu <libreliu@foxmail.com>
2021-11-10 19:36:10 +0800

    Use mysqldump instead of copying around

--- maintenance/backup.sh
+++ maintenance/backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+source `pwd`/../.env
 
 BACKUP_SUFFIX=`date -Iminutes`
 
 docker run -it --rm --name "VLab-Backup-Files" -v "$(pwd):/backup" -v verilog-oj_files-volume:/volume:ro ubuntu:20.04 tar cvf "/backup/backup-files-volume-${BACKUP_SUFFIX}.tar" /volume
 
-docker run -it --rm --name "VLab-Backup-DB" -v "$(pwd):/backup" -v verilog-oj_db-volume:/volume:ro ubuntu:20.04 tar cvf "/backup/backup-db-volume-${BACKUP_SUFFIX}.tar" /volume
+#docker run -it --rm --name "VLab-Backup-DB" -v "$(pwd):/backup" -v verilog-oj_db-volume:/volume:ro ubuntu:20.04 tar cvf "/backup/backup-db-volume-${BACKUP_SUFFIX}.tar" /volume
+docker exec verilog-oj-db-1 /usr/bin/mysqldump -u root --password=${mysql_root_password} django_db > backup-db-${BACKUP_SUFFIX}.sql
--- maintenance/restore.sh
+++ maintenance/restore.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+source `pwd`/../.env
 
 BACKUP_SUFFIX="$1"
 
-if [ -f "backup-files-volume-${BACKUP_SUFFIX}.tar" ] && [ -f "backup-db-volume-${BACKUP_SUFFIX}.tar" ]; then
+if [ -f "backup-files-volume-${BACKUP_SUFFIX}.tar" ] && [ -f "backup-db-${BACKUP_SUFFIX}.sql" ]; then
     docker run -it --rm --name "VLab-Restore-Files" -v "$(pwd):/backup" -v verilog-oj_files-volume:/volume ubuntu:20.04 tar xvf "/backup/backup-files-volume-${BACKUP_SUFFIX}.tar" -C /volume
 
-    docker run -it --rm --name "VLab-Restore-DB" -v "$(pwd):/backup" -v verilog-oj_db-volume:/volume ubuntu:20.04 bash -c "rm -rf /volume/*; tar xvf /backup/backup-db-volume-${BACKUP_SUFFIX}.tar -C /volume;"
+    #docker run -it --rm --name "VLab-Restore-DB" -v "$(pwd):/backup" -v verilog-oj_db-volume:/volume ubuntu:20.04 bash -c "rm -rf /volume/*; tar xvf /backup/backup-db-volume-${BACKUP_SUFFIX}.tar -C /volume;"
+    cat "backup-db-${BACKUP_SUFFIX}.sql" | docker exec -i verilog-oj-db-1 /usr/bin/mysql -u root --password=${mysql_root_password} django_db
 else
     echo "Suffix ${BACKUP_SUFFIX} not present"
     exit 1
