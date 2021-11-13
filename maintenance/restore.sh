31c38fe546ef71df2bcc3937bde60bb16a57a555
lluckydog <zhanghaowen@mail.ustc.edu.cn>
2021-11-13 13:45:21 +0800

    revert version

--- maintenance/restore.sh
+++ maintenance/restore.sh
@@ -4,7 +4,7 @@ source `pwd`/../.env
 BACKUP_SUFFIX="$1"
 
 if [ -f "backup-files-volume-${BACKUP_SUFFIX}.tar" ] && [ -f "backup-db-${BACKUP_SUFFIX}.sql" ]; then
-    docker run -it --rm --name "VLab-Restore-Files" -v "$(pwd):/backup" -v verilog-oj_files-volume:/volume ubuntu:20.04 tar xvf "/backup/backup-files-volume-${BACKUP_SUFFIX}.tar" -C /
+    docker run -it --rm --name "VLab-Restore-Files" -v "$(pwd):/backup" -v verilog-oj_files-volume:/volume ubuntu:20.04 tar xvf "/backup/backup-files-volume-${BACKUP_SUFFIX}.tar" -C /volume
 
     #docker run -it --rm --name "VLab-Restore-DB" -v "$(pwd):/backup" -v verilog-oj_db-volume:/volume ubuntu:20.04 bash -c "rm -rf /volume/*; tar xvf /backup/backup-db-volume-${BACKUP_SUFFIX}.tar -C /volume;"
     cat "backup-db-${BACKUP_SUFFIX}.sql" | docker exec -i verilog-oj-db-1 /usr/bin/mysql -u root --password=${mysql_root_password} django_db
