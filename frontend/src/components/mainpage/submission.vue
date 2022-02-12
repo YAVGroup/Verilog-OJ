bf963f0238c2b94399272d5d721b276ec78b56d0
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-02-13 00:49:16 +0800

    trim log for user submission

--- frontend/src/components/mainpage/submission.vue
+++ frontend/src/components/mainpage/submission.vue
@@ -69,9 +69,7 @@
                     word-wrap: break-word;
                     word-break: normal;
                   "
-                >
-                  {{ result.log }}
-                </p>
+                >{{ result.log }}</p>
 
                 <h3>波形</h3>
                 <wavedrom
