b8e7fcb43574da2544e846062cceb508ec3d5ff3
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-02-25 00:07:11 +0800

    discussion filter and information

--- backend/discussion/serializers.py
+++ backend/discussion/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Topic, Comment
+from problem.serializers import ProblemListSerializer
+from user.serializers import UserPublicListSerializer
 
 class TopicSerializer(serializers.ModelSerializer):
+    problem_belong = ProblemListSerializer(source='problem', read_only=True)
+    user_belong = UserPublicListSerializer(source='creator', read_only=True)
+
     class Meta:
         model = Topic
         
