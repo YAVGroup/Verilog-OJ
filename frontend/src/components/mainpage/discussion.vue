e83131a31eb94a4fafe62d2c9d83115a95361d86
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-02-25 18:25:50 +0800

    add addtopic

--- frontend/src/components/mainpage/discussion.vue
+++ frontend/src/components/mainpage/discussion.vue
@@ -35,11 +35,7 @@
             label="用户"
             :min-width="150"
           ></el-table-column>
-          <el-table-column
-            prop="title"
-            label="主题"
-            :min-width="150"
-          >
+          <el-table-column prop="title" label="主题" :min-width="150">
           </el-table-column>
           <el-table-column
             prop="problem_belong.name"
@@ -54,11 +50,7 @@
               </font>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="comments.length"
-            label="回复"
-            :min-width="60"
-          >
+          <el-table-column prop="comments.length" label="回复" :min-width="60">
           </el-table-column>
           <el-table-column
             prop="updatetime"
@@ -66,8 +58,56 @@
             :min-width="180"
           ></el-table-column>
         </el-table>
+        <!-- 发布帖子 -->
+        <el-card shadow="never" style="margin-top: 20px">
+          <!--提交界面-->
+          <el-row :gutter="15">
+            <i style="padding: 5px 10px" class="el-icon-edit"></i>
+            <div style="display: inline-block; font-size: 20px">
+              帖子编辑 (支持 markdown)
+            </div>
+
+            <div v-if="!loggedIn" style="display: inline-block; float: right">
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="登陆以进行提交"
+                placement="top"
+              >
+              </el-tooltip>
+            </div>
+            <div style="display: inline-block; float: right" v-else>
+              <el-button
+                type="success"
+                size="medium"
+                @click="submitTopic"
+                style="font-weight: bold; margin-right: 10px; float: right"
+                >提交</el-button
+              >
+              <el-button
+                type="danger"
+                size="medium"
+                @click="code = ''"
+                style="font-weight: bold; margin-right: 10px; float: right"
+                >清空</el-button
+              >
+            </div>
+          </el-row>
+
+          <!-- 标题编辑 -->
+          <el-row
+            >主题：
+            <el-input v-model="topictitle" placeholder="请输入标题"></el-input
+          ></el-row>
+
+          <!--内容编辑-->
+          <el-row>
+            <codemirror v-model="code" :options="cmOptions"></codemirror>
+          </el-row>
+        </el-card>
       </el-col>
     </el-row>
+
     <el-row> &nbsp; </el-row>
     <el-row>
       <el-col>
@@ -98,8 +138,16 @@
 import moment from "moment";
 import { mapState } from "vuex";
 
+import { codemirror } from "vue-codemirror";
+require("codemirror/lib/codemirror.css");
+require("codemirror/theme/base16-light.css");
+require("codemirror/mode/markdown/markdown");
+
 export default {
   name: "discussion",
+  components: {
+    codemirror,
+  },
   methods: {
     rowClick(row, col, e) {
       if (col.label == "题目") {
@@ -156,15 +204,15 @@ export default {
         (this.currentpage - 1) * this.pagesize;
       this.$axios.get(url).then((response) => {
         for (let i = 0; i < response.data.results.length; i++) {
-          let update_time = moment(
-            response.data.results[i].update_time
-          ).format("YYYY-MM-DD HH:mm:ss");
+          let update_time = moment(response.data.results[i].update_time).format(
+            "YYYY-MM-DD HH:mm:ss"
+          );
           for (const comment of response.data.results[i].comments) {
-            const comment_update_time = moment(
-              comment.update_time
-            ).format("YYYY-MM-DD HH:mm:ss");
+            const comment_update_time = moment(comment.update_time).format(
+              "YYYY-MM-DD HH:mm:ss"
+            );
             if (moment(update_time).isBefore(comment_update_time)) {
-              update_time = comment_update_time
+              update_time = comment_update_time;
             }
           }
           response.data.results[i].updatetime = update_time;
@@ -173,6 +221,43 @@ export default {
         this.totalstatus = response.data.count;
         this.loading = false;
       });
+    },
+    submitTopic: function () {
+      if (!this.loggedIn) {
+        this.$message.error("请先登录！");
+        return;
+      }
+
+      if (!this.code) {
+        this.$message.error("请输入代码！");
+        return;
+      }
+
+      this.$confirm("确定提交吗？", "提交", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "提交中...",
+          });
+
+          return this.$axios.post("/addtopic", {
+            problem: this.problemid,
+            title: this.topictitle,
+            description: this.code,
+          });
+        })
+        .then((response) => {
+          this.resetsearch();
+        })
+        .catch((error) => {
+          this.$message.error(
+            "提交失败：" + JSON.stringify(error.response.data)
+          );
+        });
     },
   },
   data() {
@@ -191,6 +276,17 @@ export default {
         problem: "",
         language: "",
       },
+
+      topictitle: "",
+      cmOptions: {
+        tabSize: 4,
+        mode: "markdown",
+        theme: "base16-light",
+        lineNumbers: true,
+        viewportMargin: Infinity,
+        lineWrapping: true,
+      },
+      code: "",
     };
   },
   computed: {
